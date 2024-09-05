6ff5a92fe8d7b2b19f796b6a19cf11e41146d8e6
Kimura Yuu <joaovictor9910@outlook.com>
2024-09-05 07:46:48 -0300

    feature: add register feature

--- src/views/Auth/Register.tsx
+++ src/views/Auth/Register.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import NewsLetter from '@/components/Landing/NewsLetter';
 import '@/assets/scss/views/Auth/Register.scss';
 
 const Register: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [conform, setConform] = useState('');
+
+    const handleRegister = () => {
+        if (email && password && conform) {
+            if (password == conform) {
+
+            } else {
+                
+            }
+        } else {
+
+        }
+    }
+
     return (
         <div className='register'>
             <h3 className='bold capslock'>New Account</h3>
             <div className='content'>
                 <Form.Group className="mb-3 w-30" controlId="formBasicEmail">
                     <Form.Label>Email address:</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        defaultValue={email}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3 w-30" controlId="formBasicEmail">
                     <Form.Label>Password:</Form.Label>
-                    <Form.Control type="password" placeholder="Enter password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter password"
+                        defaultValue={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3 w-30" controlId="formBasicEmail">
                     <Form.Label>Conform:</Form.Label>
-                    <Form.Control type="password" placeholder="Enter conform" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter conform"
+                        defaultValue={conform}
+                        onChange={e => setConform(e.target.value)}
+                    />
                 </Form.Group>
             </div>
 
@@ -29,6 +60,7 @@ const Register: React.FC = () => {
                 <Button
                     variant="danger w-30 mt-3 capslock bold"
                     type="submit"
+                    onClick={handleRegister}
                 >
                     Create account
                 </Button>
