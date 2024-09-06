e58f98fd6b9a22ecd181364588c934db2a73ced0
Kimura Yuu <joaovictor9910@outlook.com>
2024-09-06 16:34:45 -0300

    feature: add the api for login

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,9 @@ import { Credential } from '@/actions';
 const URL = 'http://127.0.0.1:9000/v1/';
 
 const register = (e: Credential) => axios.post(`${URL}auth/register`, e);
+const login = (e: Credential) => axios.post(`${URL}auth/login`, e);
 
 export default {
-    register
+    register,
+    login
 };
